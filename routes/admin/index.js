ffc483e29d73d240cebc6980bfbd3eb2bcc48e0e
Mordechai Weinberg <mordicool@gmail.com>
2017-01-23 21:49:54 +0200

    Created route for receiving logs from server, in admin page.

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -6,11 +6,13 @@ var express = require('express');
 var lecturersRoute = require('./lecturers');
 var changePasswordRoute = require('./changePassword');
 var jstreeRoute = require('./jstree');
+var logsViewRoute = require('./logsView');
 var router = express.Router();
 
 router.use('/lecturers', lecturersRoute);
 router.use('/changePassword', changePasswordRoute);
 router.use('/jstree', jstreeRoute);
+router.use('/logs', logsViewRoute);
 
 module.exports = router;
 
---
+++ routes/admin/logsView.js
@@ -0,0 +1,33 @@
+/**
+ * Created by מרדכי on 23 ינואר 2017.
+ */
+
+var config = require('../../config');
+var express = require('express');
+var fs = require('fs');
+var logger = require('../../modules/logger');
+var path = require('path');
+var router = express.Router();
+
+router.get('/', serveLogs);
+
+module.exports = router;
+
+/********************************************************************************************/
+
+function serveLogs(req, res) {
+    if (req.cookies.password !== config.authentication.adminUser.cookieValue) {
+        logger.warn('Did not change tree, do to non authenticated user. redirected to login page.');
+        res.sendStatus(400);
+    } else {
+        var logsPath = path.join(__dirname, '../../', config.logger.debugLogFileName);
+        var logs = fs.readFileSync(logsPath, 'utf8');
+
+        var logsObject = {
+            logs: logs.split('\r\n')
+        };
+
+        logger.debug('Served logs to admin page.');
+        res.json(logsObject);
+    }
+}
